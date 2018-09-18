51e59b63a95275650c8f79f913278024d05d8d91
Rodrigo V da Silva <digovs@Rodrigos-MacBook-Pro.local>
2018-09-17 21:27:41 -0400

    Log the count of GET and POST requests

--- index.js
+++ index.js
@@ -2,23 +2,37 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const port = 8090
+var getCount = 0
+var postCount = 0
 
 app.use(bodyParser.json());
 
-app.get('/sendGet', (request, response) => {
-    console.log("GET request received")
-    response.send("GET: Hello world")
+const getUri = '/sendGet'
+app.get(getUri, (request, response) => {
+    console.log(getUri + " request received")
+    getCount++
+    console.log(getRequestCountStr(getCount, postCount))
+    response.send("GET Information")
+    console.log(getUri + " sending response")
 })
 
-app.post('/sendPost', (request, response) => {
-    console.log("POST request received")
+const postUri = '/sendPost'
+app.post(postUri, (request, response) => {
+    console.log(postUri + " request received")
+    postCount++
+    console.log(getRequestCountStr(getCount, postCount))
     response.send(request.body)
+    console.log(postUri + " sending response")
 })
+
+function getRequestCountStr(getCount, postCount) {
+    return `Processed Request Count --> sendGet:${getCount}, sendPost:${postCount}`
+}
 
 app.listen(port, (err) => {
     if (err) {
         return console.log("Something bad have occurred", err)
     }
 
-    console.log("server is listening to " + port)
+    console.log("server is listening to http://127.0.0.1:" + port)
 })
