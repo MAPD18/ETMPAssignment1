f4be552c5fac19539fc11209780cc3ae68eb1264
Rodrigo V da Silva <digovs@Rodrigos-MacBook-Pro.local>
2018-09-17 20:07:23 -0400

    Installing nodemon; creating hello world for GET and POST using Express

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("NodeJS and NPM environment setup");
+const express = require('express')
+const app = express()
+const port = 8090
+
+app.get('/sendGet', (request, response) => {
+    console.log("GET request received")
+    response.send("GET: Hello world")
+})
+
+app.post('/sendPost', (request, response) => {
+    console.log("POST request received")
+    response.send("POST: Hello world")
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log("Something bad have occurred", err)
+    }
+
+    console.log("server is listening to " + port)
+})
