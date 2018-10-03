0a4f928dcfca16b3c0a0512f971351c07a389c3c
Rodrigo Silva <digovs@gmail.com>
2018-10-03 11:40:28 -0400

    Storing sendPost JSON info into a storage variable in memory

--- index.js
+++ index.js
@@ -12,27 +12,35 @@ app.get(getUri, (request, response) => {
     console.log(getUri + " request received")
     getCount++
     console.log(getRequestCountStr(getCount, postCount))
-    response.send("GET Information")
+    if (storage.length > 0) {
+        response.send(storage[0])
+    } else {
+        response.send("GET Information")
+    }
     console.log(getUri + " sending response")
 })
 
+var storage = []
 const postUri = '/sendPost'
 app.post(postUri, (request, response) => {
     console.log(postUri + " request received")
     postCount++
     console.log(getRequestCountStr(getCount, postCount))
     response.send(request.body)
+    storage.push(request.body)
     console.log(postUri + " sending response")
 })
 
 function getRequestCountStr(getCount, postCount) {
-    return `Processed Request Count --> sendGet:${getCount}, sendPost:${postCount}`
+    return `Processed Request Count --> ${getUri}:${getCount}, ${postUri}:${postCount}`
 }
 
 app.listen(port, (err) => {
     if (err) {
-        return console.log("Something bad have occurred", err)
+        return console.error("Something bad have occurred", err)
     }
 
-    console.log("server is listening to http://127.0.0.1:" + port)
+    console.info("server is listening to http://127.0.0.1:" + port)
+    console.info("server is listening to http://127.0.0.1:" + port + getUri)
+    console.info("server is listening to http://127.0.0.1:" + port + postUri)
 })
